0f8847cb5ff923e8f3adc929fcedd88ba3b84e78
jurgen <jurgen@ControlCtr.vpod.local>
2017-08-29 13:26:20 +0200

    Third Commit from local, added MySimpleSample_2

---
+++ Test-CNA/public_html/MySimpleSampleFile_2.js
@@ -0,0 +1,8 @@
+Okay that's much better'. No JavaScript Code though
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
